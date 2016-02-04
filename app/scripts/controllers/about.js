ea74dda9018facc5eb88fa7f4def639e6ee7f51a
Peter McMillan <peterm95018@gmail.com>
2016-02-03 17:52:25 -0800

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name buildingAddressesAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the buildingAddressesAppApp
+ */
+angular.module('buildingAddressesAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name buildingAddressesAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the buildingAddressesAppApp
+ */
+angular.module('buildingAddressesAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
