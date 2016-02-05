3980c04543a115723f63df6ebabc34bc5506295d
Peter McMillan <peterm@ucsc.edu>
2016-02-05 15:20:53 -0800

    yeoman generated list,edit controllers, factory file. Added ui.router and changed routing schema accordingly. Lists locations and pulls data into edit template. TODO all the other CRUD functions.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,28 +8,57 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('buildingAddressesAppApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    'ngTouch',
+    'ui.router'
+  ]);
+
+  app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('main', {
+      url: '/',
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl'
+    })
+    .state('list', {
+      url: '/list',
+      templateUrl: 'views/list.html',
+      controller: 'ListCtrl'
+    })
+    .state('edit', {
+      url: '/edit/:locationId',
+      templateUrl: 'views/edit.html',
+      controller: 'EditCtrl'
   });
+
+    $urlRouterProvider.otherwise('/');
+
+  });
+
+  // app.config(function ($routeProvider) {
+  //   $routeProvider
+  //     .when('/', {
+  //       templateUrl: 'views/main.html',
+  //       controller: 'MainCtrl',
+  //       controllerAs: 'main'
+  //     })
+  //     .when('/about', {
+  //       templateUrl: 'views/about.html',
+  //       controller: 'AboutCtrl',
+  //       controllerAs: 'about'
+  //     })
+  //     .otherwise({
+  //       redirectTo: '/'
+  //     });
+  // });
