08702faa88b562aed629e0eb1396049a4b2c0d6b
Peter McMillan <peterm95018@gmail.com>
2016-02-11 12:05:43 -0800

    clean up of ejs templates for angularjs use

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name buildingAddressesAppApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the buildingAddressesAppApp
+ */
+angular.module('buildingAddressesAppApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
