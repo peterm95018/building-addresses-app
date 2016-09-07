a37f105cafba9a363421b59f1f9896032748d270
Peter McMillan <peterm95018@gmail.com>
2016-09-07 15:12:29 -0700

    minor tweaks to files while testing sidebar and full screen vs. existing click through path

--- app/scripts/controllers/mapCtrl.js
+++ app/scripts/controllers/mapCtrl.js
@@ -12,67 +12,67 @@ angular.module('mapCtrl', ['ui-leaflet'])
   	function ($scope, $location, SharedService, leafletData) {
 
 
-    var theHash = $location.search(); // just the good stuff separated by ":"
-    var tempLatLng = theHash.c;
-    var ourResult = tempLatLng.split(":");
-    //console.log(ourResult);
+// var theHash = $location.search(); // just the good stuff separated by ":"
+// var tempLatLng = theHash.c;
+// var ourResult = tempLatLng.split(":");
+// //console.log(ourResult);
 
-$scope.ourLat = parseFloat(ourResult[0]);
-$scope.ourLng = parseFloat(ourResult[1]);
-$scope.ourTitle = $location.search().title;
+// $scope.ourLat = parseFloat(ourResult[0]);
+// $scope.ourLng = parseFloat(ourResult[1]);
+// $scope.ourTitle = $location.search().title;
 
 
-    angular.extend($scope, {
-    	center: {
-    		lat: 36.9944,
-    		lng: -122.0622,
-    		zoom: 15
-    	},
-        markers: {
-            markers: {
-            //mainMarker: angular.copy($scope.markers)
-            lat: $scope.ourLat,
-            lng: $scope.ourLng,
-            message: $scope.ourTitle,
-            draggable: false,
-            focus: true
-        }
-        },
+//     angular.extend($scope, {
+//     	center: {
+//     		lat: 36.9944,
+//     		lng: -122.0622,
+//     		zoom: 15
+//     	},
+//         markers: {
+//             markers: {
+//             //mainMarker: angular.copy($scope.markers)
+//             lat: $scope.ourLat,
+//             lng: $scope.ourLng,
+//             message: $scope.ourTitle,
+//             draggable: false,
+//             focus: true
+//         }
+//         },
 
-        defaults: {
-            scrollWheelZoom: false
-        }
-  });
+//         defaults: {
+//             scrollWheelZoom: false
+//         }
+//   });
 
 // BEGIN Shared Services rewrite
-// $scope.mapObject = SharedService.mapObject;
-// //$scope.myMarkers = {};
-// //$scope.markers = new Array();
+$scope.mapObject = SharedService.mapObject;
+//$scope.myMarkers = {};
+//$scope.markers = new Array();
 
-// $scope.$watch('mapObject["lat"]', function() {
-// 	$scope.mapObject = SharedService.mapObject;
+$scope.$watch('mapObject["lat"]', function() {
+	$scope.mapObject = SharedService.mapObject;
 
-// 	// destroy markers before recreating
-// 	// keep one marker on map at a time
-// 	$scope.markers = new Array();
+	// destroy markers before recreating
+	// keep one marker on map at a time
+	$scope.markers = new Array();
 
 	
-// 	// create the marker object
-// 	$scope.markers.push({
-// 		lat: $scope.mapObject.lat,
-// 		lng: $scope.mapObject.lng,
-// 		message: $scope.mapObject.message,
-// 		draggable: false,
-// 		focus: true
-// 	});
+	// create the marker object
+	$scope.markers.push({
+		lat: $scope.mapObject.lat,
+		lng: $scope.mapObject.lng,
+		message: $scope.mapObject.message,
+		draggable: false,
+		focus: true
+	});
 
 
-// 	// move center of map to selected items coords and zoom in
-//     leafletData.getMap().then(function(map) {
-//     	map.setView(new L.LatLng($scope.mapObject.lat, $scope.mapObject.lng),17);
-//     });
+	// move center of map to selected items coords and zoom in
+    leafletData.getMap().then(function(map) {
+    	map.setView(new L.LatLng($scope.mapObject.lat, $scope.mapObject.lng),17);
+    });
 
-// }, true);
+}, true);
 
 // END Shared Services
 
@@ -92,30 +92,30 @@ angular.extend($scope, {
 
    
 
-$scope.$on("centerUrlHash", function(event, centerHash) {
+// $scope.$on("centerUrlHash", function(event, centerHash) {
 
-    //set our title from URL
-    $scope.ourTitle = $location.search().title;
+//     //set our title from URL
+//     $scope.ourTitle = $location.search().title;
 
-    // centerHash is a format from map directive map?c:lat:lng:z
-    var theHash = centerHash;
-    var ourResult = theHash.split(":");
+//     // centerHash is a format from map directive map?c:lat:lng:z
+//     var theHash = centerHash;
+//     var ourResult = theHash.split(":");
 
-    $scope.markers = [];
-    $scope.markers.push({
-        lat: parseFloat(ourResult[0]),
-        lng: parseFloat(ourResult[1]),
-        message: $scope.ourTitle,
-        draggable: false
-    });
+//     $scope.markers = [];
+//     $scope.markers.push({
+//         lat: parseFloat(ourResult[0]),
+//         lng: parseFloat(ourResult[1]),
+//         message: $scope.ourTitle,
+//         draggable: false
+//     });
 
 
-	$location.search({ c: centerHash });
-});
+// 	$location.search({ c: centerHash });
+// });
 
-    $scope.changeLocation = function(centerHash) {
-            $location.search({ c: centerHash });
-    };
+//     $scope.changeLocation = function(centerHash) {
+//             $location.search({ c: centerHash });
+//     };
 
 
 }]);
